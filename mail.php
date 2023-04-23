f79f02e971aaa0db6c62562f4a818ae2d09b074e
ChristianRms28 <ramosca@students.nu-baliwag.edu.ph>
2023-04-24 03:04:33 +0800

    updated

--- mail.php
+++ mail.php
@@ -17,7 +17,7 @@ if(isset($_POST["send"])){
         <meta charset="UTF-8">
     </head>
     <body>
-        <p> Good Day! <strong> '. $name.'! </strong></p>
+        <p> Good Day po! <strong> '. $name.'! </strong></p>
         <p>Thank you for your feedback!</p>
         <br>
         A flower a day makes a brighter day!<br>
