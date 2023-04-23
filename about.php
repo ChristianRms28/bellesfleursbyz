ce8818b91514956f6739692ba167f15344f4d530
ChristianRms28 <ramosca@students.nu-baliwag.edu.ph>
2023-04-24 00:51:19 +0800

    first commit

---
+++ about.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Belles Fleurs</title>
+  <link rel="stylesheet" href="style.css">
+  <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
+
+  <link href="img/log.jpg" rel="icon">
+
+</head>
+<body>  
+
+  <div class="navbar">
+    <div class="inner-width">
+      <a href="#" class="logo"><span>F</span>lowers</a>
+      <div class="navbar-menu">
+        <a href="index.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="products.php">Products</a>
+        <a href='response.php'>Response</a>
+        
+      </div>
+    </div>
+  </div>
+  <section id="home">
+    <div class="content1">
+      <div class="content">
+        <h1>Belles fleurs is a simple </h>
+        
+      </div>
+      <div class="social-container">
+        <div class="social-icons">
+          <li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
+          <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+        </div>
+      </div>
+    </div>
+    <div class="content2">
+      <h1>About Belles Fleurs <span></span></h1>
+    </div>
+  </section>
+</body>
+</html>
---
+++ mail.php
@@ -0,0 +1,49 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+if(isset($_POST["send"])){
+    $email = $_POST["email"];
+    $name = $_POST["name"];
+    $subject = $_POST["subject"];
+
+    $msg ='<!DOCTYPE html>
+    <html lang="en"> 
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <p> Good Day! <strong> '. $name.'! </strong></p>
+        <p>Thank you for your feedback!</p>
+        <br>
+        A flower a day makes a brighter day!<br>
+        <strong></b>Belles Fleurs 🌸</strong>
+
+    </body>
+    </html>';
+
+
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    $mail->Username = "christianrms00@gmail.com";
+    $mail->Password = "rlhycywfauiwdesz";
+    $mail->SMTPSecure = "tls";
+    $mail->Port = "587";
+
+    $mail->setFrom("christianrms00@gmail.com","Belles Fleurs");
+    $mail->addAddress($email);
+    $mail->isHTML(true);
+
+    $mail->Subject = $subject;
+    $mail->Body = $msg;
+
+    $mail->send();
+    header('location:index.php');
+}
+?>
