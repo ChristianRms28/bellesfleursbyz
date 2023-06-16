bc934603d62effaec05c114d5263d2466e6a20d3
ChristianRms28 <ramosca@students.nu-baliwag.edu.ph>
2023-06-16 09:46:46 +0800

    finaleeeee

--- crudmain/index.php
+++ crudmain/index.php
@@ -7,82 +7,73 @@
     <title>Belles Fleurs</title>
     <link rel="stylesheet" href="style.css">
     <link href="ImageBf/log.jpg" rel="icon">
- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
-  <style>
-  input{
-    margin: 10px;
-}
-body{
-background-color: #FFB6C1;
-}
-            </style>
-
-</head>
-<body>
-        <center>
-        <div class="main">
-        <form action="insert.php" method="POST" enctype="multipart/form-data" >
-        <label for="">Name:</label>
-        <input type="text" name="name"required><br>
-        <label for="">Price :</label>
-        <input type="text" name="price" id=""required><br>
-        <label for="">Image:</label>
-        <input type="file" name="image" id=""required><br>
-        <button type="submit" name="upload">Upload</button>
-
-        </form>
-    </div>
-        </center>
-
-        <!-- fetch data -->
-
-        <div class="container">
-
-        <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Name</th>
-      <th scope="col">Price</th>
-      <th scope="col">Image</th>
-      <th scope="col">Delete</th>
-      <th scope="col">Update</th>
-      
-     
-      
-      
-      
-      
-    </tr>
-  </thead>
-  <tbody>
-     
-        <?php
-        include 'config.php';
-        $pic = mysqli_query($con,"SELECT * FROM `image`");
-        while($row = mysqli_fetch_array($pic)){
-        echo "
-            <tr>
-                <td>$row[id]</td>
-                <td>$row[Name]</td>
-                <td>$row[Price]</td>
-                <td><img src='$row[Image]'  width = '200px'  height = '200px'></td>
-                <td><a href='delete.php? Id= $row[id]' class = 'btn btn-danger'>Delete</a></td>
-                <td><a href='update.php? Id= $row[id]' class = 'btn btn-danger'>Update</a></td>
-                <td></td>
-                
-                
-                
-            </tr>
-            ";
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    
+    <style>
+        input {
+            margin: 10px;
         }
         
-        ?>
+        .container {
+            background-image: url("bg2.jpg");
+            background-size: cover;
+            background-position: center;
+        }
+        
+        body {
+            background-color: #F2DADF;
+            height: 100vh;
+        }
+    </style>
+</head>
+<body>
+    <center>
+        <div class="main">
+            <form action="insert.php" method="POST" enctype="multipart/form-data">
+                <label for="">Name:</label>
+                <input type="text" name="name" required><br>
+                <label for="">Price:</label>
+                <input type="text" name="price" id="" required><br>
+                <label for="">Image:</label>
+                <input type="file" name="image" id="" required><br>
+                <button type="submit" name="upload">Upload</button>
+            </form>
+        </div>
+    </center>
 
-
-  </tbody>
-</table>
-</div>
+    <!-- fetch data -->
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Image</th>
+                    <th scope="col">Delete</th>
+                    <th scope="col">Update</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                include 'config.php';
+                $pic = mysqli_query($con, "SELECT * FROM `image`");
+                while ($row = mysqli_fetch_array($pic)) {
+                    echo "
+                        <tr>
+                            <td>$row[id]</td>
+                            <td>$row[Name]</td>
+                            <td>$row[Price]</td>
+                            <td><img src='$row[Image]' width='200px' height='200px'></td>
+                            <td><a href='delete.php? Id=$row[id]' class='btn btn-danger'>Delete</a></td>
+                            <td><a href='update.php? Id=$row[id]' class='btn btn-danger'>Update</a></td>
+                            <td></td>
+                        </tr>
+                    ";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
